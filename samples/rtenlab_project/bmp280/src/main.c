2283595238ce64c387cf2a3ef14c57f78abe2198
Dev Joshi <devjoshi9031@gmail.com>
2021-09-29 20:10:05 -0700

    BMP280 done. Both Temperature and Pressure sensor working perfectly with the default settings as is from adafruit_bmp280 library

--- samples/rtenlab_project/bmp280/src/main.c
+++ samples/rtenlab_project/bmp280/src/main.c
@@ -24,21 +24,21 @@
 
 
 #define I2C_DEV "I2C_0"
-#define mask(x) (1<<x)									// Macro for masking bits.
+#define mask(x) (1<<x)																				// Macro for masking bits.
 #define DEBUG 0
 
 uint8_t data_bmp280=1;
-const struct device *dev_bmp280;						// Device struct to get device binding for I2C
-uint8_t BMP280_ADDR = 0x77;									// Address of the Sensor on I2C bus as described by Adafruit website
-uint8_t temp_data[3];												// Array to hold the raw temperature data.
-uint8_t press_data[3];											// Array to hold the raw pressure data.
-uint32_t temperature, pressure;							// 32 bit variable to convert the 20 bit data from the array of registers to a single variable
-uint8_t calib_data[2];											// dump array of registers to burst read the 16 bit calib data.
-uint16_t calib_data_unsigned_registers[2];												//Contains dig_T1 and dig_P1 register.
+const struct device *dev_bmp280;															// Device struct to get device binding for I2C
+uint8_t BMP280_ADDR = 0x77;																		// Address of the Sensor on I2C bus as described by Adafruit website
+uint8_t temp_data[3];																					// Array to hold the raw temperature data.
+uint8_t press_data[3];																				// Array to hold the raw pressure data.
+uint32_t temperature, pressure;																// 32 bit variable to convert the 20 bit data from the array of registers to a single variable
+uint8_t calib_data[2];																				// dump array of registers to burst read the 16 bit calib data.
+uint16_t calib_data_unsigned_registers[2];										//Contains dig_T1 and dig_P1 register.
 int16_t calib_data_signed_registers[10];											// Containes dig_T2,T3 and dig_[P2-P9] registers.
 uint8_t calib_signed_registers[10]= {0x8A,0x8C,0x90,0x92,
-						0x94,0x96,0x98,0x9A,0x9C,0x9E};	// Register address to get the calibaration data.
-uint8_t calib_unsigned_registers[2] = {0x88,0x8E};				// Register address to get the unsigned calibration data.
+						0x94,0x96,0x98,0x9A,0x9C,0x9E};										// Register address to get the calibaration data.
+uint8_t calib_unsigned_registers[2] = {0x88,0x8E};						// Register address to get the unsigned calibration data.
 int32_t t_fine;
 
 /**
@@ -72,7 +72,7 @@ void read_calibration_registers(void){
 	calib_data[0]=0;calib_data[1]=0;
 	}
 
-	// FOr loop to get the signed data dig_T2-T3 and dig_P[2-9].
+	// For loop to get the signed data dig_T2-T3 and dig_P[2-9].
 	for(int i=0;i<10;i++){
 	i2c_burst_read(dev_bmp280,BMP280_ADDR,calib_signed_registers[i],&calib_data[0],2);
 	calib_data_signed_registers[i] = ((int16_t)((int16_t)(calib_data[1]))<<8 | ((int16_t)(calib_data[0])));
@@ -116,12 +116,14 @@ int32_t pressure_read_raw_data(void){
 	rawdata = (int32_t) ((((uint32_t) (press_data[0])) << 12) | (((uint32_t) (press_data[1])) << 4) | ((uint32_t) press_data[2] >> 4));
 	return rawdata;
 }
+
 /**
 * FUNCTION: To convert the raw data into human readable format. Adapted from the adafruit library.
 */
+
 int32_t read_pressure(void){
 	int64_t var1, var2, p;
-
+	read_temperature();																			// Required to get the t_fine variable.
 	int32_t press = pressure_read_raw_data();
 
 	var1 = ((int64_t)t_fine) - 128000;
@@ -143,13 +145,20 @@ int32_t read_pressure(void){
 	var2 = (((int64_t)calib_data_signed_registers[8]) * p) >> 19;
 
 	p = ((p + var1 + var2) >> 8) + (((int64_t) calib_data_signed_registers[7]) << 4);
+	printk("Value of p: %lld\n",p);
 	return p;
 }
+
+/**
+* FUNCTION: Main function!
+*/
+
 void main(void){
 
 	dev_bmp280 = device_get_binding(I2C_DEV);
-	const struct device  *dev_usb;						// Device for USB Console.
+	const struct device  *dev_usb;																										// Device for USB Console.
 	uint32_t dtr=0;
+	uint8_t control = 182;																														// Binary 0b10110110 equivalent to 182 in decimal. Pressure_Sampling: 0x16 and Tempreature_Sampling: 0x16.
 
 	dev_usb = device_get_binding(CONFIG_UART_CONSOLE_ON_DEV_NAME);
 	if(usb_enable(NULL))
@@ -168,21 +177,20 @@ void main(void){
 
     i2c_configure(dev_bmp280, I2C_SPEED_SET(I2C_SPEED_STANDARD));
    
-	i2c_reg_read_byte(dev_bmp280,0x77,0xD0,&data_bmp280);			// This command the configure the APDS9960 sensor and enables the proximity and ALS property on the device.
+	i2c_reg_read_byte(dev_bmp280,0x77,0xD0,&data_bmp280);															// This command the configure the APDS9960 sensor and enables the proximity and ALS property on the device.
 	if(data_bmp280==88)
 		printk("Bosch BMP280 Found!\n");
+
   }
 	 i2c_reg_write_byte(dev_bmp280,BMP280_ADDR,0xF5,0);
 	
 	while(1){
-	i2c_reg_write_byte(dev_bmp280,BMP280_ADDR,0xF4,182);  
+	i2c_reg_write_byte(dev_bmp280,BMP280_ADDR,0xF4,control);  
 	k_sleep(K_MSEC(500));	
-	int32_t final_temperature = read_temperature();
+	int32_t final_temperature = read_temperature();							
 	int32_t final_pressure = read_pressure();
-
 	int32_t dec = final_temperature%100;	
 	printk("Tempreature data: %d.%2d\t Pressure: %d\n",final_temperature/100,dec,final_pressure/256); 
 	}
-
   return;
 }
