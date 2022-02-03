552a51b809418065a01a811f089bbf244ebf28ab
Dev Joshi <devjoshi9031@gmail.com>
2022-02-02 16:32:45 -0800

    Sensor is still not working

--- samples/rtenlab_project/blinky/src/main.c
+++ samples/rtenlab_project/blinky/src/main.c
@@ -16,20 +16,8 @@
 #define SLEEP_TIME_MS   1000
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-#define LED1_NODE DT_ALIAS(led2)
 
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define LED0_PIN		DT_GPIO_PIN(LED0_NODE, gpios)
-#define LED0_FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
-#else
-/* A build error here means your board isn't set up to blink an LED. */
-#error "Unsupported board: led0 devicetree alias is not defined"
-#define LED0	""
-#define LED0_PIN	0
-#define LED0_FLAGS	0
-#endif
+#define LED1_NODE DT_ALIAS(led2)
 
 #if DT_NODE_HAS_STATUS(LED1_NODE, okay)
 #define LED1	DT_GPIO_LABEL(LED1_NODE, gpios)
@@ -45,33 +33,76 @@
 #define LOW 0
 #define HIGH 1
 
-const struct device *dev0, *dev1;
-
-
-uint8_t read_bit(void){
-	uint8_t r;
-	gpio_pin_set(dev1,LED1_PIN,LOW);
-	k_usleep(3);
-	gpio_pin_set(dev1,LED1_PIN,HIGH);
-	k_usleep(10);
-	r = gpio_pin_get(dev1,LED1_PIN);
-	k_usleep(53);
-	return r;
+static inline void udelay(int secs){
+	k_busy_wait(secs);
 }
 
+const struct device *dev0, *dev1;
+
+uint8_t reset(void){
+	int key = irq_lock();
+	uint8_t r;
+	gpio_pin_configure(dev1, LED1_PIN, GPIO_OUTPUT);
+	gpio_pin_set(dev1,LED1_PIN,LOW);
+	udelay(480);
+	gpio_pin_set(dev1,LED1_PIN,HIGH);
+	udelay(70);
+	// while((gpio_pin_get(dev1,LED1_PIN))!=0){
+	// 	r = gpio_pin_get(dev1,LED1_PIN);
+	// 	printk("stuck in teh while loopwith value of r: %d\n",r);
+	// }
+	gpio_pin_configure(dev1, LED1_PIN, GPIO_INPUT);
+	r = gpio_pin_get(dev1,LED1_PIN);
+	udelay(410);
+	irq_unlock(key);
+	return !r;
+}
+
+// uint8_t read_bit(void){
+// 	uint8_t r;
+// 	gpio_pin_configure(dev1, LED1_PIN, GPIO_OUTPUT);
+// 	gpio_pin_set(dev1,LED1_PIN,LOW);
+// 	udelay(3);
+// 	gpio_pin_set_raw(dev1,LED1_PIN,HIGH);
+// 	udelay(13);
+// 	gpio_pin_configure(dev1, LED1_PIN, GPIO_INPUT);
+// 	r = gpio_pin_get(dev1,LED1_PIN);
+// 	udelay(53);
+// 	return r;
+// }
+
+// uint8_t read(void){
+// 	uint8_t bitMask;
+// 	uint8_t r = 0;
+
+// 	for(bitMask=0x01; bitMask;bitMask<<=1){
+// 		if(read_bit())
+// 			r|=bitMask;
+// 	}
+// 	return r;
+// }
+
 void write_bit(uint8_t v){
+	// printk("Outputting bit: %d\n",v);
 	if(v&1){
+		// int key = irq_lock();
+		gpio_pin_configure(dev1, LED1_PIN, GPIO_OUTPUT);
 		gpio_pin_set(dev1,LED1_PIN,LOW);
-		k_usleep(10);
+		udelay(10);
 		gpio_pin_set(dev1,LED1_PIN,HIGH);
-		k_usleep(55);
+		// irq_unlock(key);
+		udelay(55);
 	}
 	else{
+		// int key = irq_lock();
+		gpio_pin_configure(dev1, LED1_PIN, GPIO_OUTPUT);
 		gpio_pin_set(dev1,LED1_PIN,LOW);
-		k_usleep(65);
+		udelay(65);
 		gpio_pin_set(dev1,LED1_PIN,HIGH);
-		k_usleep(5);
+		// irq_unlock(key);
+		udelay(5);
 	}
+	return;	
 }
 
 void write(uint8_t v){
@@ -82,57 +113,35 @@ void write(uint8_t v){
 	return;
 }
 
-uint8_t read(void){
-	uint8_t bitMask;
-	uint8_t r = 0;
 
-	for(bitMask=0x01; bitMask;bitMask<<=1){
-		if(read_bit())
-			r|=bitMask;
-	}
-	return r;
-}
 void main(void)
 {
 	int ret;
-	enable_uart_console();
+	// enable_uart_console();
 
-	dev0 = device_get_binding(LED0);
-	if (dev0 == NULL) {
-		return;
-	}
 	dev1 = device_get_binding(LED1);
 	if (dev1 == NULL) {
 		return;
 	}
 
-	ret = gpio_pin_configure(dev0, LED0_PIN, GPIO_OUTPUT_ACTIVE | LED0_FLAGS);
-	if (ret < 0) {
-		return;
-	}
-
-	ret = gpio_pin_configure(dev1, LED1_PIN, GPIO_INPUT | GPIO_PULL_UP | GPIO_ACTIVE_HIGH);
+	ret = gpio_pin_configure(dev1, LED1_PIN, GPIO_OUTPUT);
 	if (ret < 0) {
 		return;
 	}
 	
-	write(0x33);
-	uint8_t retu = read();
-	printf("The Rom address is as follows: %d\n", retu);
-	// while (1) {
-
-	// 	// gpio_pin_set(dev0, LED0_PIN, (int)led_is_on);
-	// 	// led_is_on = !led_is_on;
-	// 	// k_usleep(1000000);
-	// 	// gpio_pin_set(dev0, LED0_PIN, (int)led_is_on);
-	// 	// k_usleep(1000000);
-	// 	// printk("Dev is a great person...\n");
-	// 	// while((gpio_pin_get(dev1,LED1_PIN))<=0){
-	// 	// 	val = gpio_pin_get(dev1,LED1_PIN);
-	// 	// printk("waiting for a rise with value of val: %d\n", val);
-	// 	// k_usleep(1000);
-	// 	// }
-	// 	// val = gpio_pin_get(dev1,LED1_PIN);
-	// 	// printk("The value at D5 is: %d\n", val);
-	// }
+	while(1){
+	reset();
+	// write(0x33);
+	// gpio_pin_set(dev1, LED1_PIN, LOW);
+	// delay(100);
+	// write(0x10);
+	// gpio_pin_set(dev1,LED1_PIN, HIGH);
+	// delay(100);
+	}
+	// write(0xFF);
+	
+	// uint8_t retu = read();
+	// printf("The Rom address is as follows: 0x%x\n", retu);
+	// retu=read();
+	// printf("The Rom address is as follows: 0x%x\n", retu);
 }
--- samples/rtenlab_project/simple_blinky/src/main.c
+++ samples/rtenlab_project/simple_blinky/src/main.c
@@ -8,12 +8,13 @@
 #include <device.h>
 #include <devicetree.h>
 #include <drivers/gpio.h>
+#include "uart_i2c.h"
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#define LED0_NODE DT_ALIAS(led2)
 
 #if DT_NODE_HAS_STATUS(LED0_NODE, okay)
 #define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
@@ -27,8 +28,11 @@
 #define FLAGS	0
 #endif
 
+
+
 void main(void)
 {
+	// enable_uart_console();
 	const struct device *dev;
 	bool led_is_on = true;
 	int ret;
@@ -38,14 +42,19 @@ void main(void)
 		return;
 	}
 
-	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return;
 	}
-
 	while (1) {
-		gpio_pin_set(dev, PIN, (int)led_is_on);
-		led_is_on = !led_is_on;
-		k_busy_wait(10000);
+		gpio_pin_configure(dev, PIN, GPIO_OUTPUT);
+		gpio_pin_set(dev, PIN, (int)true);
+		k_busy_wait(480);
+		gpio_pin_set(dev,PIN,(int)false);
+		k_busy_wait(1);		
+		gpio_pin_configure(dev, PIN, GPIO_INPUT);
+		int r = gpio_pin_get(dev, PIN);	
+		// printf("%d\n",r);
+		k_busy_wait(10);
 	}
 }
