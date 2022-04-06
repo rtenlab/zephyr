9fe30c60fb84db9635d394ba06dc4c46e3974908
Dev Joshi <devjoshi9031@gmail.com>
2022-04-05 20:23:19 -0700

    Working to get the LSM, SCD, DS integrated with influxdb and grafana

--- samples/rtenlab_project/all_sensors_bluetooth_peripheral/src/main.c
+++ samples/rtenlab_project/all_sensors_bluetooth_peripheral/src/main.c
@@ -35,8 +35,8 @@
 #define APDS9960
 #define BMP280
 #define LSM6DS33
-// #define SCD41
-// #define DS18B20
+#define SCD41
+#define DS18B20
 
 // Defines to get the format of the data sent using custom characteristics UUID
 #define CPF_FORMAT_UINT8 	0x04
@@ -62,6 +62,25 @@ static struct bt_uuid_128 sht_uuid = BT_UUID_INIT_128(
 // @brief  UUID for apds sensor data: ebcc60b7-974c-43e1-a973-426e79f9bc6c 
 static struct bt_uuid_128 apds_uuid = BT_UUID_INIT_128(
 		BT_UUID_128_ENCODE(0xebcc60b7, 0x974c, 0x43e1, 0Xa973,  0X426e79f9bc6c));
+
+
+//  @brief  UUID for apds sensor data: 1441e94a-74bc-4412-b45b-f1d91487afe5
+static struct bt_uuid_128 apds_prox_uuid = BT_UUID_INIT_128(
+		BT_UUID_128_ENCODE(0x1441e94a, 0x74bc, 0x4412, 0Xb45b,  0X4f1d91487afe5));
+
+
+//  @brief  UUID for red_als apds sensor data: 3c321537-4b8e-4662-93f9-cb7df0e437c5
+static struct bt_uuid_128 apds_als_red_uuid = BT_UUID_INIT_128(
+		BT_UUID_128_ENCODE(0x3c321537, 0x4b8e, 0x4662, 0x93f9, 0xcb7df0e437c5));
+
+
+//  @brief  UUID for blue_als apds sensor data: 47024a73-790e-48ba-aac4-7d9e018572ba
+static struct bt_uuid_128 apds_als_blue_uuid = BT_UUID_INIT_128(
+		BT_UUID_128_ENCODE(0x47024a73, 0x790e, 0x48ba, 0xaac4, 0x7d9e018572ba));
+
+// 	@brief  UUID for green_als apds sensor data: 2f15eb47-9512-4ce3-8897-2f4460df7be4
+static struct bt_uuid_128 apds_als_green_uuid = BT_UUID_INIT_128(
+		BT_UUID_128_ENCODE(0x2f15eb47, 0x9512, 0x4ce3, 0x8897, 0x2f4460df7be4));
 
 // 	@brief  UUID for clear_als apds sensor data: e960c9b7-e0ed-441e-b22c-d93252fa0fc6
 
@@ -199,8 +218,35 @@ BT_GATT_SERVICE_DEFINE(ess_svc,
 #ifdef APDS9960
 // Primary Service for APDS9960 sensor.
 	BT_GATT_PRIMARY_SERVICE(&apds_uuid),
+// Characteristic proximity value and descriptors for unit and format. attr[8]
+	BT_GATT_CHARACTERISTIC(&apds_prox_uuid.uuid, BT_GATT_CHRC_NOTIFY,
+			       BT_GATT_PERM_READ, NULL, NULL, NULL),	
+	BT_GATT_CCC(hrmc_ccc_cfg_changed,
+		    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+	BT_GATT_CPF(&proximity),
 
-// Characteristic blue_als data value and descriptors for unit and format. attr[8]
+// Characteristic red_als data value and descriptors for unit and format. att[12]
+	BT_GATT_CHARACTERISTIC(&apds_als_red_uuid.uuid, BT_GATT_CHRC_NOTIFY,
+					BT_GATT_PERM_READ, NULL, NULL, NULL),
+	BT_GATT_CCC(hrmc_ccc_cfg_changed,
+			BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+	BT_GATT_CPF(&als),
+
+// Characteristic green_als data value and descriptors for unit and format. attr[16]
+	BT_GATT_CHARACTERISTIC(&apds_als_green_uuid.uuid, BT_GATT_CHRC_NOTIFY,
+					BT_GATT_PERM_READ, NULL, NULL, NULL),
+	BT_GATT_CCC(hrmc_ccc_cfg_changed,
+			BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+	BT_GATT_CPF(&als),
+
+// Characteristic blue_als data value and descriptors for unit and format. attr[20]
+	BT_GATT_CHARACTERISTIC(&apds_als_blue_uuid.uuid, BT_GATT_CHRC_NOTIFY,
+					BT_GATT_PERM_READ, NULL, NULL, NULL),
+	BT_GATT_CCC(hrmc_ccc_cfg_changed,
+			BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+	BT_GATT_CPF(&als),
+
+// Characteristic blue_als data value and descriptors for unit and format. attr[12]
 	BT_GATT_CHARACTERISTIC(&apds_als_clear_uuid.uuid, BT_GATT_CHRC_NOTIFY,
 					BT_GATT_PERM_READ, NULL, NULL, NULL),
 	BT_GATT_CCC(hrmc_ccc_cfg_changed,
@@ -210,13 +256,13 @@ BT_GATT_SERVICE_DEFINE(ess_svc,
 
 #ifdef BMP280
 	BT_GATT_PRIMARY_SERVICE(&bmp280_primary_uuid),
-// Caharactersitic temp. value. attrs[13]
+// Caharactersitic temp. value. attrs[29]
 	BT_GATT_CHARACTERISTIC(BT_UUID_TEMPERATURE, BT_GATT_CHRC_NOTIFY,
 			       BT_GATT_PERM_READ, NULL, NULL, NULL),
 	BT_GATT_CCC(hrmc_ccc_cfg_changed,
 		    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 
-// charactersitic humidity value. attrs[16]
+// charactersitic humidity value. attrs[32]
 	BT_GATT_CHARACTERISTIC(BT_UUID_PRESSURE, BT_GATT_CHRC_NOTIFY,
 					BT_GATT_PERM_READ, NULL, NULL, NULL),
 	BT_GATT_CCC(hrmc_ccc_cfg_changed,
@@ -225,46 +271,67 @@ BT_GATT_SERVICE_DEFINE(ess_svc,
 
 #ifdef LSM6DS33
 	BT_GATT_PRIMARY_SERVICE(&lsm6ds33_primary_uuid),
-// Characteristic green_als data value and descriptors for unit and format. attr[20]
+// Characteristic green_als data value and descriptors for unit and format. attr[36]
 	BT_GATT_CHARACTERISTIC(&lsm6ds33_accl_x_uuid.uuid, BT_GATT_CHRC_NOTIFY,
 					BT_GATT_PERM_READ, NULL, NULL, NULL),
 	BT_GATT_CCC(hrmc_ccc_cfg_changed,
 			BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 	BT_GATT_CPF(&accel),
 
-// Characteristic blue_als data value and descriptors for unit and format. attr[24]
+// Characteristic blue_als data value and descriptors for unit and format. attr[40]
 	BT_GATT_CHARACTERISTIC(&lsm6ds33_accl_y_uuid.uuid, BT_GATT_CHRC_NOTIFY,
 					BT_GATT_PERM_READ, NULL, NULL, NULL),
 	BT_GATT_CCC(hrmc_ccc_cfg_changed,
 			BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 	BT_GATT_CPF(&accel),
 
-// Characteristic blue_als data value and descriptors for unit and format. attr[28]
+// Characteristic blue_als data value and descriptors for unit and format. attr[44]
 	BT_GATT_CHARACTERISTIC(&lsm6ds33_accl_z_uuid.uuid, BT_GATT_CHRC_NOTIFY,
 					BT_GATT_PERM_READ, NULL, NULL, NULL),
 	BT_GATT_CCC(hrmc_ccc_cfg_changed,
 			BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 	BT_GATT_CPF(&accel),
 
+// Characteristic green_als data value and descriptors for unit and format. attr[48]
+	BT_GATT_CHARACTERISTIC(&lsm6ds33_gyro_x_uuid.uuid, BT_GATT_CHRC_NOTIFY,
+					BT_GATT_PERM_READ, NULL, NULL, NULL),
+	BT_GATT_CCC(hrmc_ccc_cfg_changed,
+			BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+	BT_GATT_CPF(&gyro),
+
+// Characteristic blue_als data value and descriptors for unit and format. attr[52]
+	BT_GATT_CHARACTERISTIC(&lsm6ds33_gyro_y_uuid.uuid, BT_GATT_CHRC_NOTIFY,
+					BT_GATT_PERM_READ, NULL, NULL, NULL),
+	BT_GATT_CCC(hrmc_ccc_cfg_changed,
+			BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+	BT_GATT_CPF(&gyro),
+
+// Characteristic blue_als data value and descriptors for unit and format. attr[56]
+	BT_GATT_CHARACTERISTIC(&lsm6ds33_gyro_z_uuid.uuid, BT_GATT_CHRC_NOTIFY,
+					BT_GATT_PERM_READ, NULL, NULL, NULL),
+	BT_GATT_CCC(hrmc_ccc_cfg_changed,
+			BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+	BT_GATT_CPF(&gyro),
+
 #endif
 
 #ifdef SCD41
 	BT_GATT_PRIMARY_SERVICE(&scd41_primary_uuid),
-// Caharactersitic Co2 value. attrs[33]
+// Caharactersitic Co2 value. attrs[61]
 	BT_GATT_CHARACTERISTIC(&scd41_CO2_uuid.uuid, BT_GATT_CHRC_NOTIFY,
 			       BT_GATT_PERM_READ, NULL, NULL, NULL),
 	BT_GATT_CCC(hrmc_ccc_cfg_changed,
 		    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 	BT_GATT_CPF(&als),
 
-// charactersitic humidity value. attrs[37]
+// charactersitic humidity value. attrs[65]
 	BT_GATT_CHARACTERISTIC(BT_UUID_HUMIDITY, BT_GATT_CHRC_NOTIFY,
 					BT_GATT_PERM_READ, NULL, NULL, NULL),
 
 	BT_GATT_CCC(hrmc_ccc_cfg_changed,
 		    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 
-// charactersitic temperature value. attrs[40]
+// charactersitic temperature value. attrs[68]
 	BT_GATT_CHARACTERISTIC(BT_UUID_TEMPERATURE, BT_GATT_CHRC_NOTIFY,
 					BT_GATT_PERM_READ, NULL, NULL, NULL),
 
@@ -274,7 +341,7 @@ BT_GATT_SERVICE_DEFINE(ess_svc,
 
 #ifdef DS18B20
 	BT_GATT_PRIMARY_SERVICE(&ds18b_primary_uuid),
-// Caharactersitic Co2 value. attrs[44]
+// Caharactersitic Co2 value. attrs[72]
 	BT_GATT_CHARACTERISTIC(BT_UUID_TEMPERATURE, BT_GATT_CHRC_NOTIFY,
 			       BT_GATT_PERM_READ, NULL, NULL, NULL),
 	BT_GATT_CCC(hrmc_ccc_cfg_changed,
@@ -313,10 +380,19 @@ void apds9960_notify(void)
 	static apds9960_t sensor_value;
 	uint8_t prox;
 	int16_t red, green, blue, clear;
+	read_proximity_data(&sensor_value);
 	read_als_data(&sensor_value);
+	prox = sensor_value.proximity;
+	red = sensor_value.red;
+	green = sensor_value.green;
+	blue = sensor_value.blue;
 	clear = sensor_value.clear;
 
-	bt_gatt_notify(NULL, &ess_svc.attrs[8], &sensor_value.clear, sizeof(sensor_value.clear));
+	bt_gatt_notify(NULL, &ess_svc.attrs[8], &sensor_value.proximity, sizeof(sensor_value.proximity));
+	bt_gatt_notify(NULL, &ess_svc.attrs[12], &sensor_value.red, sizeof(sensor_value.proximity));
+	bt_gatt_notify(NULL, &ess_svc.attrs[16], &sensor_value.green, sizeof(sensor_value.proximity));
+	bt_gatt_notify(NULL, &ess_svc.attrs[20], &sensor_value.blue, sizeof(sensor_value.proximity));
+	bt_gatt_notify(NULL, &ess_svc.attrs[24], &sensor_value.clear, sizeof(sensor_value.proximity));
 	return;
 }
 #endif
@@ -330,8 +406,8 @@ void bmp280_notify(void){
 	bmp_read_press_temp_data(&sensor_value);
 	temperature = (uint16_t)((sensor_value.temperature)*100);
 	pressure= (uint32_t)((sensor_value.pressure)*10);
-	bt_gatt_notify(NULL, &ess_svc.attrs[13], &temperature, sizeof(temperature));
-	bt_gatt_notify(NULL, &ess_svc.attrs[16], &pressure, sizeof(pressure));
+	bt_gatt_notify(NULL, &ess_svc.attrs[29], &temperature, sizeof(temperature));
+	bt_gatt_notify(NULL, &ess_svc.attrs[32], &pressure, sizeof(pressure));
 	return;
 }
 #endif
@@ -341,16 +417,29 @@ void lsm6ds33_notify(void){
 	static lsm6ds33_t sensor_value;
 	int16_t gyroX, gyroY, gyroZ, accelX, accelY, accelZ;
 	read_burst_data(&sensor_value);
+	gyroX = (int16_t) (((sensor_value.gyroX)*100)+32768);
+	gyroY = (int16_t) ((sensor_value.gyroY)*100+32768);
+	gyroZ = (int16_t) ((sensor_value.gyroZ)*100+32768);
 
 	accelX = (int16_t)((sensor_value.accelX)*100+32768);
 	accelY = (int16_t)((sensor_value.accelY)*100+32768);
 	accelZ = (int16_t)((sensor_value.accelZ)*100+32768);
 
+	// gyroX = ~(gyroX-1);
+	// gyroY = ~(gyroY-1);
+	// gyroZ = ~(gyroZ-1);
 
-	bt_gatt_notify(NULL, &ess_svc.attrs[20], &accelX, sizeof(accelX));
-	bt_gatt_notify(NULL, &ess_svc.attrs[24], &accelY, sizeof(accelY));
-	bt_gatt_notify(NULL, &ess_svc.attrs[28], &accelZ, sizeof(accelZ));
+	// accelX = ~(accelX-1);
+	// accelY = ~(accelY-1);
+	// accelZ = ~(accelZ-1);
 
+	bt_gatt_notify(NULL, &ess_svc.attrs[36], &accelX, sizeof(accelX));
+	bt_gatt_notify(NULL, &ess_svc.attrs[40], &accelY, sizeof(accelY));
+	bt_gatt_notify(NULL, &ess_svc.attrs[44], &accelZ, sizeof(accelZ));
+
+	bt_gatt_notify(NULL, &ess_svc.attrs[48], &gyroX, sizeof(gyroX));
+	bt_gatt_notify(NULL, &ess_svc.attrs[52], &gyroY, sizeof(gyroY));
+	bt_gatt_notify(NULL, &ess_svc.attrs[56], &gyroZ, sizeof(gyroZ));
 	return;
 }
 #endif
@@ -365,9 +454,9 @@ void scd41_notify(void){
 	uint16_t temp, hum;
 	temp = (uint16_t)(sensor_value.temp*100);
 	hum = (uint16_t)(sensor_value.hum*100);
-	bt_gatt_notify(NULL, &ess_svc.attrs[33], &Co2, sizeof(Co2));
-	bt_gatt_notify(NULL, &ess_svc.attrs[37], &hum, sizeof(hum));
-	bt_gatt_notify(NULL, &ess_svc.attrs[40], &temp, sizeof(temp));
+	bt_gatt_notify(NULL, &ess_svc.attrs[61], &Co2, sizeof(Co2));
+	bt_gatt_notify(NULL, &ess_svc.attrs[65], &hum, sizeof(hum));
+	bt_gatt_notify(NULL, &ess_svc.attrs[68], &temp, sizeof(temp));
 }
 
 #endif
@@ -380,7 +469,7 @@ void ds18b_notify(void){
 	sensor_value = getTempCByIndex(0);
 	static uint16_t some;
 	some = (uint16_t)((sensor_value)*100);
-	bt_gatt_notify(NULL, &ess_svc.attrs[44], &some, sizeof(some));
+	bt_gatt_notify(NULL, &ess_svc.attrs[72], &some, sizeof(some));
 	return;
 
 }
@@ -403,11 +492,6 @@ static void connected(struct bt_conn *conn, uint8_t err)
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
-	int err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
-		return;
-	}
 	printk("Disconnected (reason 0x%02x)\n", reason);
 }
 
