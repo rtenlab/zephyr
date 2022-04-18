368671de95def89e6ee4bc48b0de8e64605163fe
Dev Joshi <devjoshi9031@gmail.com>
2022-04-18 10:26:41 -0700

    Worked a bit on msg queue with sync_dev_implementation

--- samples/rtenlab_project/synchronization_dev_implementation/src/main.c
+++ samples/rtenlab_project/synchronization_dev_implementation/src/main.c
@@ -9,12 +9,14 @@
 // #include "uart_i2c.c"
 #include <zephyr.h>
 #include <sys/printk.h>
+#include <errno.h>
 
 #include "sht31.c"
 #include "uart_i2c.c"
 #include "lsm6ds33.c"
 #include "apds9960.c"
 #include "scd41.c"
+
 
 
 /*
@@ -47,10 +49,18 @@
 
 // K_SEM_DEFINE(threadA_sem, 1, 1);	/* starts off "available" */
 // K_SEM_DEFINE(threadB_sem, 0, 1);	/* starts off "not available" */
+enum data_type{
+	SENSOR_SHT31,
+	SENSOR_APDS9960,
+	SENSOR_BMP280,
+	SENSOR_LSM6DS33,
+	SENSOR_SCD41,
+	SENSOR_DS18B20,
+};
 
 typedef struct {
 	// id->0: sht data; id->1: apds data
-	uint8_t id;
+	enum data_type type;
 	union 
 	{
 		// scd41_t scd41_data;
@@ -64,30 +74,10 @@ typedef struct {
 
 
 struct k_mutex mymutex;
-ble_data_t shared_buffer[10];
 
-void scd41(void *dummy1, void *dummy2, void* dummy3)
-{
-	ARG_UNUSED(dummy1);
-	ARG_UNUSED(dummy2);
-	ARG_UNUSED(dummy3);
-	struct k_thread *current_thread;
-	current_thread = k_current_get();
-	scd41_t threadD_scd41;
-	
-	while(1){
-		k_mutex_lock(&mymutex,K_FOREVER);
-		printk("Hello from %s\n",k_thread_name_get(current_thread));
-		measure_single_shot(&threadD_scd41);
-		print_data_scd(&threadD_scd41);
-		k_msleep(SLEEPTIME);
-		k_mutex_unlock(&mymutex);
-	}
-
-}
-
-
-
+/* Create a MSG Queue for the threads to use for data passing*/
+// struct k_msgq my_msgq;
+K_MSGQ_DEFINE(my_msgq, sizeof(ble_data_t), 3, 4);
 
 void apds9960(void *dummy1, void *dummy2, void *dummy3)
 {
@@ -98,12 +88,15 @@ void apds9960(void *dummy1, void *dummy2, void *dummy3)
 	// apds9960_t threadC_apds9960;
 	current_thread = k_current_get();
 		// Write something to start another sensor.
+		ble_data_t apds_local_data;
+		apds_local_data.type = SENSOR_APDS9960;
 	while(1){
 		k_mutex_lock(&mymutex, K_FOREVER);
 		printk("Hello from %s\n", k_thread_name_get(current_thread));
 		// read_proximity_data(&threadC_apds9960);
-		read_als_data(&shared_buffer[1].apds_cls_data);
-		print_data_apds(&shared_buffer[1].apds_cls_data);
+		read_als_data(&apds_local_data.apds_cls_data);
+		k_msgq_put(&my_msgq, &apds_local_data, K_FOREVER);
+		// print_data_apds(&shared_buffer[1].apds_cls_data);
 		k_msleep(SLEEPTIME);
 		k_mutex_unlock(&mymutex);
 		k_sleep(K_FOREVER);
@@ -113,26 +106,7 @@ void apds9960(void *dummy1, void *dummy2, void *dummy3)
 }
 
 
-/* threadB is a dynamic thread that is spawned by threadA */
 
-void lsm6ds33(void *dummy1, void *dummy2, void *dummy3)
-{
-	ARG_UNUSED(dummy1);
-	ARG_UNUSED(dummy2);
-	ARG_UNUSED(dummy3);
-	struct k_thread *current_thread;
-	current_thread = k_current_get();
-	while(1){
-	/* invoke routine to ping-pong hello messages with threadA */
-	lsm6ds33_t threadB_lsm6ds33;
-	k_mutex_lock(&mymutex, K_FOREVER);
-	printk("Hello from %s\n", k_thread_name_get(current_thread));
-	k_msleep(SLEEPTIME);
-	read_burst_data(&threadB_lsm6ds33);
-	print_data(&threadB_lsm6ds33);
-	k_mutex_unlock(&mymutex);
-	}
-}
 
 /* threadA is a static thread that is spawned automatically */
 
@@ -142,33 +116,58 @@ void sht31(void *dummy1, void *dummy2, void *dummy3)
 	ARG_UNUSED(dummy2);
 	ARG_UNUSED(dummy3);
 	/* invoke routine to ping-pong hello messages with threadB */
+	struct k_thread *current_thread;
+	current_thread = k_current_get();
+	ble_data_t sht31_local_data;
+	sht31_local_data.type = SENSOR_SHT31;
 	while(1){
-		struct k_thread *current_thread;
-		current_thread = k_current_get();
 		k_mutex_lock(&mymutex, K_FOREVER);
 		printk("Hello from %s\n", k_thread_name_get(current_thread));
 		k_msleep(SLEEPTIME);
-		read_temp_hum(&shared_buffer[1].sht31_data);
-		print_data_sht(&shared_buffer[1].sht31_data);
+		read_temp_hum(&sht31_local_data.sht31_data);
+		k_msgq_put(&my_msgq, &sht31_local_data, K_FOREVER);
+		// print_data_sht(&shared_buffer[1].sht31_data);
 		k_mutex_unlock(&mymutex);
 		k_sleep(K_FOREVER);
 	}
 	return;
 }
 
+char* enum_to_string(ble_data_t* data){
+	if(data->type == SENSOR_APDS9960){
+		return "APDS9960";
+	}
+	else if(data->type == SENSOR_SHT31){
+		return "SHT31";
+	}
+	return "NOT VALID";
+}
+
+void consumer_thread(void* dummy1, void* dummy2, void* dummy3)
+{
+	ARG_UNUSED(dummy1);
+	ARG_UNUSED(dummy2);
+	ARG_UNUSED(dummy3);
+	ble_data_t consumer_local_data;
+	while(1){
+		int8_t ret = k_msgq_get(&my_msgq, &consumer_local_data, K_MSEC(10));
+		if(ret!=0){
+			printk("Couldn't find data\n");
+		}
+		printk("Data type: %s\t Data value: (Nothing for now)\n", enum_to_string(&consumer_local_data));
+		k_sleep(K_FOREVER);
+	}
+
+}
 
 K_THREAD_STACK_DEFINE(sht31_stack_area, STACKSIZE);
-static struct k_thread sht31_data;
-
-K_THREAD_STACK_DEFINE(lsm6ds33_stack_area, STACKSIZE);
-static struct k_thread lsm6ds33_data;
+static struct k_thread sht31_thread_data;
 
 K_THREAD_STACK_DEFINE(apds9960_stack_area, STACKSIZE);
-static struct k_thread apds9960_data;
+static struct k_thread apds9960_thread_data;
 
-K_THREAD_STACK_DEFINE(scd41_stack_area, STACKSIZE);
-static struct k_thread scd41_data;
-
+K_THREAD_STACK_DEFINE(consumer_stack_area, STACKSIZE);
+static struct k_thread consumer_thread_data;
 
 /**
  * @brief  Work Handler function. This will be ran by a thread in backend depending on the priority.
@@ -177,9 +176,10 @@ static struct k_thread scd41_data;
  * @retval None
  */
 void my_work_handler(struct k_work *work){
-	k_thread_resume(&sht31_data);
+	k_thread_resume(&sht31_thread_data);
 	// k_thread_start(&lsm6ds33_data);
-	k_thread_resume(&apds9960_data);
+	k_thread_resume(&apds9960_thread_data);
+	k_thread_resume(&consumer_thread_data);
 }
 
 // Define a work and it's work handler
@@ -203,36 +203,27 @@ void main(void)
 	k_mutex_init(&mymutex);
 	printk("Number of CPUS: %d\n", CONFIG_MP_NUM_CPUS);
 
-	k_thread_create(&sht31_data, sht31_stack_area,
+	k_thread_create(&sht31_thread_data, sht31_stack_area,
 			K_THREAD_STACK_SIZEOF(sht31_stack_area),
 			sht31, NULL, NULL, NULL,
 			PRIORITY, 0, K_MSEC(100));
-	k_thread_name_set(&sht31_data, "thread_A");
+	k_thread_name_set(&sht31_thread_data, "SHT_Thread");
 
 
 
-	k_thread_create(&apds9960_data, apds9960_stack_area, 
+	k_thread_create(&apds9960_thread_data, apds9960_stack_area, 
 			K_THREAD_STACK_SIZEOF(apds9960_stack_area),
 			apds9960, NULL, NULL, NULL, 
 			PRIORITY, 0, K_MSEC(100));
-	k_thread_name_set(&apds9960_data, "thread_C");
-
-
-	// k_thread_create(&lsm6ds33_data, lsm6ds33_stack_area,
-	// 		K_THREAD_STACK_SIZEOF(lsm6ds33_stack_area),
-	// 		lsm6ds33, NULL, NULL, NULL,
-	// 		PRIORITY, 0, K_FOREVER);
-	// k_thread_name_set(&lsm6ds33_data, "thread_B");
-
-	// k_thread_create(&scd41_data, scd41_stack_area, 
-	// 		K_THREAD_STACK_SIZEOF(scd41_stack_area), 
-	// 		scd41, NULL, NULL, NULL,
-	// 		PRIORITY, 0, K_FOREVER);
-	// k_thread_name_set(&scd41_data, "thread_D");
+	k_thread_name_set(&apds9960_thread_data, "APDS Thread");
 
 	k_timer_start(&my_timer, K_SECONDS(2), K_SECONDS(4));
 
 
-	
-	// k_thread_start(&scd41_data);
+	k_thread_create(&consumer_thread_data, consumer_stack_area,
+			K_THREAD_STACK_SIZEOF(consumer_stack_area),
+			consumer_thread, NULL, NULL, NULL,
+			PRIORITY-3, 0, K_MSEC(100));
+	k_thread_name_set(&consumer_thread_data, "Consumer_Thread");
+
 }
